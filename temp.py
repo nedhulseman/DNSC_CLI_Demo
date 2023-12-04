828b83fe079ecfc01e0b1908b0606e881c92de4e
edward.hulseman@gwu.edu <>
2023-12-04 16:44:06 -0500

    Inialized repo, added temp.py for tracking

---
+++ temp.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+a = 10
+
+#%%
+b = 10
+import pandas as pd
+df = pd.DataFrame({"a": [1,2,3], "b":[4,5,6]})
+
+#%%
+
+def add(x, y):
+    return x + y
